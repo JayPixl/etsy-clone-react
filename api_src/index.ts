4a1c3a62337f5879df58cf17571b91044ec9a1de
Joshua Lawrence, Jr <jaypixl@protonmail.com>
2023-10-09 22:15:56 -0400

    Bug Fixes

--- api_src/index.ts
+++ api_src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { prisma } from './utils/prisma.js'
+import { prisma } from './utils/_prisma.js'
 const app = express()
 
 app.use(express.json())
