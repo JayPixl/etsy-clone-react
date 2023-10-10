93b91c245da339676a34c49d696c4645d2606739
Joshua Lawrence, Jr <jaypixl@protonmail.com>
2023-10-09 21:59:01 -0400

    Changed path to prisma.js for bug fixing

--- api_src/index.ts
+++ api_src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { prisma } from './prisma.js'
+import { prisma } from './utils/prisma.js'
 const app = express()
 
 app.use(express.json())
